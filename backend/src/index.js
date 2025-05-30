d7a11de081846cbb1771de59672ae2ee28cb550d
Peter-DevX <peterasiedugyan0@gmail.com>
2025-05-30 20:03:22 +0000

    minor change

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,6 @@ import { app, server } from "./lib/socket.js";
 
 import path from "path";
 
-
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -22,12 +21,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/chat-app/dist")));
 
-  app.get("/files{/*path}", (req,res) =>{
-    res.sendFile(path.join(__dirname, "../frontend/chat-app", "dist", "index.html"));
-  })
+  app.get("/files/*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/chat-app", "dist", "index.html")
+    );
+  });
 }
 
 server.listen(PORT, () => {
