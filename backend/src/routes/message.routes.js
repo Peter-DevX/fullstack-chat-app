b5d63913764ffa11fca46b8e15f6bb6c7029dcc9
Peter-DevX <peterasiedugyan0@gmail.com>
2025-05-30 19:52:06 +0000

    enhanced message routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -3,13 +3,37 @@ import { protectRoute } from "../middleware/auth.protectRoute.js";
 import {
   getUsersForSidebar,
   getMessages,
-  sendMessage
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users/", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
-router.post("/send/:id", protectRoute, sendMessage)
+router.get(
+  "/:id",
+  protectRoute,
+  (req, res, next) => {
+    if (!req.params.id) {
+      return res
+        .status(400)
+        .json({ error: "Missing message user ID parameter." });
+    }
+    next();
+  },
+  getMessages
+);
+router.post(
+  "/send/:id",
+  protectRoute,
+  (req, res, next) => {
+    if (!req.params.id) {
+      return res
+        .status(400)
+        .json({ error: "Missing message user ID parameter." });
+    }
+    next();
+  },
+  sendMessage
+);
 
 export default router;
